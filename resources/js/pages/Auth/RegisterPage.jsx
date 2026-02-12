3881582c12cfc1af1a8966ba75898b548ceb68e7
Christopher Francisco <christopherfrancisco171@gmail.com>
2026-02-12 14:31:03 +0800

    new

--- resources/js/pages/Auth/RegisterPage.jsx
+++ resources/js/pages/Auth/RegisterPage.jsx
@@ -3,6 +3,7 @@ import BG_IMAGE_URL from "../../assets/images/building.png";
 import SCHOOL_LOGO from "../../assets/images/site_logo.svg";
 import ILLUSTRATION from "../../assets/images/vector.png";
 import apiService from "../../services/apiService";
+import Button from "../../components/common/Button";
 
 const inputStyle =
     "w-full border border-gray-300 rounded-md px-3 py-2 text-sm bg-white focus:outline-none focus:ring-2 focus:ring-green-500";
