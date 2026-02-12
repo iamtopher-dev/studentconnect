5fcaeec27ce6024b6a7821dbd3f475eedbeaa373
Christopher Francisco <christopherfrancisco171@gmail.com>
2026-02-12 13:49:35 +0800

    new

--- resources/js/pages/Staff/StudentGradingPage.jsx
+++ resources/js/pages/Staff/StudentGradingPage.jsx
@@ -173,7 +173,7 @@ const StudentGradingPage = () => {
             .post(`staff/release-grades-students/${userId}`, { subjects: subjects })
             .then((response) => {
                 let result = response.data;
-                console(result.status ? "success":"warning")
+                console.log(result.status ? "success":"warning")
                 Swal.fire({
                     title: "Releasing Grades",
                     text: result.message,
