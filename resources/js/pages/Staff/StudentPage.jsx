b73b0fd54fab717651764e4c489d4f0fe30bfe49
Christopher Francisco <christopherfrancisco171@gmail.com>
2026-02-22 21:03:58 +0800

    new

--- resources/js/pages/Staff/StudentPage.jsx
+++ resources/js/pages/Staff/StudentPage.jsx
@@ -183,7 +183,7 @@ const StudentPage = () => {
         <div className="p-4 sm:p-6 bg-gray-50 min-h-screen">
             <div className="mb-6">
                 <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800">
-                    Student Lists
+                    Student List
                 </h1>
                 <p className="text-sm sm:text-base text-gray-400">
                     Home / Students
